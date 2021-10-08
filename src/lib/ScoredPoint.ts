5a5ff3344b9c992ecca66b137a08834c6015ad24
Ronny Acácio <ronnyacacio27@gmail.com>
2021-10-08 09:59:46 -0300

    feat: add response log

--- src/lib/ScoredPoint.ts
+++ src/lib/ScoredPoint.ts
@@ -10,7 +10,8 @@ export async function scoredPoint() {
     wifi: true
   };
 
-  await qrpoint.post('checkpoint', point, { headers });
-  
+  const response = await qrpoint.post('checkpoint', point, { headers });
+
+  console.log('🚀 ~ file: ScoredPoint.ts ~ line 15 ~ scoredPoint ~ response', response.data);
   console.log('Scored Point! ✅');
 }
